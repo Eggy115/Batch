5b7c5e49dde2081def40922642658acce4661776
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-15 17:20:01 +0000

    Create

---
+++ arrays.bat
@@ -0,0 +1,7 @@
+@echo off
+set a[0] = 1 
+set a[1] = 2 
+set a[2] = 3 
+echo The first element of the array is %a[0]% 
+echo The second element of the array is %a[1]% 
+echo The third element of the array is %a[2]%
